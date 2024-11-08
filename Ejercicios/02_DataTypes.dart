8dd5978d6d1e0f0ff873a655a5abb875c1460539
Sebastian Medina <JSebastian19952011@gmail.com>
2024-11-07 23:02:40 -0500

    Manejo de tipos de datos

--- Ejercicios/02_DataTypes.dart
+++ Ejercicios/02_DataTypes.dart
@@ -36,4 +36,49 @@ main() {
   String core4 = "Toro";
   String completeCores = "$core1 $core2 $core3 $core4";
   print(completeCores);
+  // ===== Booleans
+  bool isActive = true;
+  bool isNoActive = !isActive; //Negación
+  //bool? candidateExample;
+  print(isActive);
+  print(isNoActive);
+  //print(candidateExample!); //Cuando le coloco el ! al final es decir que confíe que no será null
+  // ===== Lists
+  print("______________________________");
+  print("______________________________");
+  List<String> heros = ["IronMan", "Thor", "Capitan America"];
+  print(heros);
+  heros[1] = "Hulk";
+  heros.add("Pantera Negra"); //Insertar al final
+  print(heros);
+  print("______________________________");
+  print("______________________________");
+  // ===== Sets
+  Set<String> villains = {
+    "Thanos",
+    "Ultrón",
+    "Loki",
+    "Doom"
+  }; //Lo mismo list pero no tiene duplicados
+  villains.add("Dormamut");
+  print(villains);
+  print("______________________________");
+  print("______________________________");
+  // ===== Maps     -   Diccionarios / Objetos
+  Map<String, dynamic> ironman = {
+    //SON PARES DE VALORES
+    "name": "Tony Stark",
+    "skill": "Inteligencia y Dinero",
+    "power": "Armadura",
+    "nivel": 9000
+  };
+
+  print(ironman);
+  print(ironman['name']);
+
+  Map<String, dynamic> thanos = new Map();
+  thanos
+      .addAll({"name": "Thanos", "power": "Guantele Infinito", "nivel": 56000});
+
+  print(thanos);
 }
